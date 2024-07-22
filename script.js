8727ac8439a4b8a74ea394e18b895915631e22b2
Nikoloz Kekelidze <nikakekelidze29@gmail.com>
2024-07-22 18:52:54 +0400

    g
    g

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ let xMark = document.querySelector(".fa-xmark");
 let mobileMenu = document.querySelector(".header_nav");
 
 burger.addEventListener("click", ()=>{
-    mobileMenu.classList.add(".show");
+    mobileMenu.classList.add("show");
     burger.style.display = "none";
     xMark.style.display = "block";
 });
 
 xMark.addEventListener("click", ()=>{
-    mobileMenu.classList.remove(".show");
+    mobileMenu.classList.remove("show");
     burger.style.display = "block";
     xMark.style.display = "none";
 });
